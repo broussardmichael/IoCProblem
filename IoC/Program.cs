a74eced7aa57d3887e1198cf721ef895d8942fd0
michaelbroussard <michael_broussard@msn.com>
2020-11-11 13:53:28 -0600

    Creating my IoC problem

---
+++ IoC/Program.cs
@@ -0,0 +1,42 @@
+﻿//Convert to loosely coupled using IoC
+
+using System;
+
+namespace IoC
+{
+    class Program
+    {
+        public class CustomerBusinessLogic
+        {
+            DataAccess _dataAccess;
+
+            public CustomerBusinessLogic()
+            {
+                _dataAccess = new DataAccess();
+            }
+
+            public string GetCustomerName(int id)
+            {
+                return _dataAccess.GetCustomerName(id);
+            }
+        }
+
+        public class DataAccess
+        {
+            public DataAccess()
+            {
+            }
+
+            public string GetCustomerName(int id)
+            {
+                return "Dummy Customer Name"; // get it from DB in real app
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CustomerBusinessLogic logic = new CustomerBusinessLogic();
+            Console.WriteLine(logic.GetCustomerName(1));
+        }
+    }
+}
